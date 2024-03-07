9f12f4146f7329648a994591fede106acb926de6
Harshitha Mohan <harshitha.mohan@esko.com>
2024-03-07 17:56:54 +0530

    Error Handling V1

--- Globaltimezone.cpp
+++ Globaltimezone.cpp
@@ -4,70 +4,13 @@
 #include <ctime>
 #include <map>
 #include <sstream>
+#include <algorithm>
 #include <time.h>
+#include "TimeZoneManager.cpp"
 
 
 using namespace std;
-class TimeZoneConverter {
-private:
-	map<string, int> timeZoneOffsets;
-public:
-	TimeZoneConverter() {
-		timeZoneOffsets["UTC"] = 0;
-		timeZoneOffsets["PST"] = -8; // Pacific Standard Time
-		timeZoneOffsets["EST"] = -5; // Eastern Standard Time
-		timeZoneOffsets["CST"] = -6; // Central Standard Time
-		timeZoneOffsets["MST"] = -7; // Mountain Standard Time
-		timeZoneOffsets["HST"] = -10; // Hawaii-Aleutian Standard Time
-		timeZoneOffsets["AKST"] = -9; // Alaska Standard Time
-		timeZoneOffsets["AEST"] = 10; // Australian Eastern Standard Time
-		timeZoneOffsets["JST"] = 9; // Japan Standard Time
-		timeZoneOffsets["IST"] = 5.5; // Indian Standard Time
-		timeZoneOffsets["BST"] = 1; // British Summer Time
-		timeZoneOffsets["CET"] = 1; // Central European Time
 
-
-	}
-
-	string getCurrentTime(const string& timeZone) {
-		time_t now = time(0);
-		tm localtime;
-		gmtime_s(&localtime, &now);
-		localtime.tm_hour += timeZoneOffsets[timeZone];
-		mktime(&localtime);
-
-		char buffer[80];
-		strftime(buffer, sizeof(buffer), "%Y-%m-%d  %H:%M:%S", &localtime);
-		return buffer;
-	}
-
-	string convertTime(string& sourceTimeZone, string& targetTimeZone, string& time) {
-		int sourceOffset = timeZoneOffsets[sourceTimeZone];
-		int targetOffset = timeZoneOffsets[targetTimeZone];
-
-		int year, month, day, hour, minute, second;
-		char colon;
-		istringstream ss(time);
-		ss >> year >> colon >> month >> colon >> day >> hour >> colon >> minute >> colon >> second;
-
-		tm tm = {};
-		tm.tm_year = year - 1900; // Years since 1900
-		tm.tm_mon = month - 1;     // Month index is 0-based
-		tm.tm_mday = day;
-		tm.tm_hour = hour;
-		tm.tm_min = minute;
-		tm.tm_sec = second;
-
-		time_t rawTime = mktime(&tm);
-		rawTime += (targetOffset - sourceOffset) * 3600;
-		tm = *localtime(&rawTime);
-
-		char buffer[80];
-		strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", &tm);
-		return buffer;
-	}
-
-};
 class Globaltimezone {
 private:
 	TimeZoneConverter converter;
@@ -93,7 +36,7 @@ public:
 	}
 	
 	void displayMenu() {
-		cout << "Welcome to the Global Time Zone Application \n"
+		cout << "\nWelcome to the Global Time Zone Application \n"
 			<< " 1. View current time in different time zones \n "
 			<< " 2. Convert time between different time zones \n"
 			<< " 3. Toggle daylight savings time on or off. \n"
@@ -128,7 +71,21 @@ public:
 		string timezone;
 		cout << "Enter the name of the timezone\n";
 		cin >> timezone;
-		cout << "Current time in " << timezone << ":" << converter.getCurrentTime(timezone) << "\n\n";
+		if (!timezone.empty() && find_if(timezone.begin(), timezone.end(), [](char c) {
+			return !isdigit(c);
+			}) == timezone.end()) {
+			if (converter.timeZoneOffsets.find(timezone) != converter.timeZoneOffsets.end()) {
+				cout << "Current time in " << timezone << ":" << converter.getCurrentTime(timezone) << "\n\n";
+			}
+			else {
+				cout << "Invalid timezone.\n";
+			}
+		}
+		else {
+			cout << "Invalid Input.Please input a valid time zone";
+			exit(1);
+			}
+		
 	}
 
 	void convertTime() {
--- TimeZoneManager.cpp
+++ TimeZoneManager.cpp
@@ -1,20 +1,29 @@
-/*#include <iostream>
+#include <iostream>
+#include <fstream>
 #include <string>
 #include <ctime>
 #include <map>
 #include <sstream>
 #include <time.h>
 
-
 using namespace std;
 class TimeZoneConverter {
-private:
+public:
 	map<string, int> timeZoneOffsets;
 public:
 	TimeZoneConverter() {
 		timeZoneOffsets["UTC"] = 0;
-		timeZoneOffsets["PST"] = -8;
-		timeZoneOffsets["EST"] = -5;
+		timeZoneOffsets["PST"] = -8; // Pacific Standard Time
+		timeZoneOffsets["EST"] = -5; // Eastern Standard Time
+		timeZoneOffsets["CST"] = -6; // Central Standard Time
+		timeZoneOffsets["HST"] = -10; // Hawaii-Aleutian Standard Time
+		timeZoneOffsets["AKST"] = -9; // Alaska Standard Time
+		timeZoneOffsets["AEST"] = 10; // Australian Eastern Standard Time
+		timeZoneOffsets["JST"] = 9; // Japan Standard Time
+		timeZoneOffsets["IST"] = 5.5; // Indian Standard Time
+		timeZoneOffsets["BST"] = 1; // British Summer Time
+		timeZoneOffsets["CET"] = 1; // Central European Time
+
 
 	}
 
@@ -57,138 +66,3 @@ public:
 	}
 
 };
-class Globaltimezone {
-private:
-	TimeZoneConverter converter;
-
-	map<int, string> favoriteTimeZones;
-public:
-	void displayMenu() {
-		cout << "Welcome to the Global Time Zone Applicatio! \n\n"
-			<< " 1. View current time in different time zones \n "
-			<< " 2. Convert time between different time zones \n"
-			<< " 3. Toggle daylight savings time on or off. \n"
-			<< " 4. Manage fav time zones \n"
-			<< " 5. Help and info \n"
-			<< " 6. Exit .\n\n"
-			<< "Enter your choice";
-	}
-	void executeChoice(int choice) {
-		switch (choice) {
-		case 1:
-			viewCurrentTime(); break;
-		case 2:
-			convertTime(); break;
-		case 3:
-			toggleDayLight();
-		case 4:
-			manageFavoriteTimeZones();
-			break;
-		case 5:
-			displayHelp();
-			break;
-		default:
-			cout << "invalid choice";
-		}
-	}
-
-	void viewCurrentTime() {
-		string timezone;
-		cout << "Enter the name of the timezone";
-		cin >> timezone;
-		cout << "Current time in " << timezone << ":" << converter.getCurrentTime(timezone) << "\n\n";
-	}
-
-	void convertTime() {
-		string sourceTimeZone, targetTimeZone, time;
-		cout << "Enter the source time";
-		cin >> sourceTimeZone;
-		cout << "Enter the target time zone";
-		cin >> targetTimeZone;
-		cout << "Enter the time (YYYY-MM-DD HH:MM:SS): ";
-		cin.ignore();
-		getline(cin, time);
-		cout << "Converted time in " << targetTimeZone << ": " << converter.convertTime(sourceTimeZone, targetTimeZone, time) << "\n\n";
-
-
-
-	}
-
-	void toggleDayLight() {
-		string choice;
-		cout << "Enable or diable DST?(on/off)";
-		cin >> choice;
-		cout << "DST" << choice << "for the time zone";
-
-	}
-
-	void manageFavoriteTimeZones() {
-		int choice;
-		cout << "1. Add fav time zone. \n"
-			<< "2. Remove fav time zone. \n"
-			<< "3. View fav time zone \n"
-			<< "4. to main menu. \n"
-			<< "5. Enter choice \n";
-		cin >> choice;
-		cout << "\n";
-
-		switch (choice) {
-		case 1:
-			addFavTimeZ(); break;
-		case 2:
-			removeFavTimeZone(); break;
-		case 3:
-			viewFavTimeZone(); break;
-		case 4:
-			break;
-
-		}
-
-	}
-
-	void addFavTimeZ() {
-		string timezone;
-		cout << " Enter the name of the timezone";
-		cin >> timezone;
-		favoriteTimeZones.insert(pair<int, string>(favoriteTimeZones.size() + 1, timezone));
-		cout << "Time zone added to favorites.\n\n";
-
-	}
-
-	void removeFavTimeZone() {
-		int index;
-		cout << "enter the index of the time zone to be removed";
-		cin >> index;
-		if (index > 0 && index < favoriteTimeZones.size()) {
-			cout << "Removed no. " << index << "Successfully";
-		}
-		else {
-			cout << "Invalid index \n\n";
-		}
-	}
-
-	void viewFavTimeZone() {
-		cout << "Favourite timezones are: ";
-		for (const auto& pair : favoriteTimeZones) {
-			cout << pair.first << "." << pair.second << "\n";
-		}
-		cout << "\n";
-	}
-	void displayHelp() {
-		cout << "Help & Information:\n"
-			<< "- This application allows you to view current time in different time zones, convert time between time zones, toggle daylight savings time adjustments, and manage favorite time zones.\n"
-			<< "- When converting time between time zones, please enter the time in the format: YYYY-MM-DD HH:MM:SS\n"
-			<< "- You can add or remove time zones from your favorites for quick access.\n\n";
-	}
-};
-int main() {
-	Globaltimezone app;
-	int choice;
-	while (true) {
-		app.displayMenu();
-		cin >> choice;
-		cout << "\n";
-		app.executeChoice(choice);
-	}
-
-}*/
